ca79afb7692ca3094fbf4801e49c5750923ae0e7
KM <36287726+mccab22k@users.noreply.github.com>
2021-12-06 15:21:05 -0500

    Pinger try again

--- solution.py
+++ solution.py
@@ -135,9 +135,9 @@ def ping(host, timeout=1):
 		print(delay)
 		time.sleep(1)  # one second
 		# array=[i]
-		array[i]=delay #first loop at 1, then 2, then 3 ...
+		# array[i]=delay #first loop at 1, then 2, then 3 ...
 		# array.insert(i,int(delay*1000))
-		# array.append(delay)
+		array.append(delay)
 	# Calculate vars values and return them
 	print("printing array: ")
 	print(array)
