254958c91b7deeda64e7c276ad21c029e34f116d
KM <36287726+mccab22k@users.noreply.github.com>
2021-12-06 15:18:23 -0500

    Pinger try again

--- solution.py
+++ solution.py
@@ -130,7 +130,7 @@ def ping(host, timeout=1):
 
 	array=[]
 	# Send ping requests to a server separated by approximately one second
-	for int(i) in [0,1,2,3,4]:
+	for i in [0,1,2,3,4]:
 		delay = doOnePing(dest, timeout)*1000
 		print(delay)
 		time.sleep(1)  # one second
